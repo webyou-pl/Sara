c840d9ec7c830b1313847399da61292c298e5900
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2019-04-20 22:17:17 +0200

    continuation of the rationalization

--- subpages/kontakt.php
+++ subpages/kontakt.php
@@ -17,12 +17,15 @@
                 
                 <div class="row subpage-contact">
                     <div class="col-4 subpage-contact__address">
-                        {dane adresowe}
-                        <img src="'../../../images/sygnet-sara.svg" alt="sygnet sara">
-                        <strong>Sara Nowodworska</strong> 
-                        <p>Nyska 61A<br/> 50-505 Wrocław</p>
-                        <p>512 567 200</p>
-                        <div class="socjal-media">
+                        <div class="subpage-contact__address--logo">
+                            <img src="'../../../images/sygnet-sara.svg" alt="Sara Nowodworska" class="img-fluid">
+                        </div>
+                        <div class="subpage-contact__address--address">
+                            <p><strong>Sara Nowodworska</strong></p> 
+                            <p>Nyska 61A<br/> 50-505 Wrocław</p>
+                            <p>512 567 200</p>
+                        </div>
+                        <div class="subpage-contact__address--social">
                             <a target="_blank" href="https://www.facebook.com/Sara-Nowodworska-Sara-Nails-2113993628851184">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" version="1.1">
                                     <g id="surface1">
@@ -58,7 +61,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col-8 subpage-contact__form">
+                    <div class="col-6 offset-md-1 subpage-contact__form">
                         <form>
                             <input type="email">
                             <input type="tel">
@@ -66,9 +69,11 @@
                                 At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies. 
                             </textarea>
                             <input type="checkbox">
-                            <input type="submit" value="Submit">
+                            <input class="btn btn-primary" type="submit" value="Submit">
                         </form>
                     </div>
+                </div>
+                <div class="row">
                     <div class="col-12 subpage-contact__gmap">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3544.3408238899156!2d17.051897672424307!3d51.085810127861144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd219ba0dee9742d8!2sSara+Nowodworska+Sara+Nails!5e0!3m2!1spl!2spl!4v1555750197582!5m2!1spl!2spl" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
